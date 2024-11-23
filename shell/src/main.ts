22b818d9adf97d9a0128488088ab74d97ec8c1bd
Leonardo Merza <ljmerza@gmail.com>
2024-11-23 16:33:35 -0500

    yrs

--- shell/src/main.ts
+++ shell/src/main.ts
@@ -1,8 +1,4 @@
 import { loadRemoteEntry } from '@angular-architects/module-federation';
 
-Promise.all([
-   loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:4202/remoteEntry.js' })
-])
-.catch(err => console.error('Error loading remote entries', err))
-.then(() => import('./bootstrap'))
+import('./bootstrap')
 .catch(err => console.error(err));
